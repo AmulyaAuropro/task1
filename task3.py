a8fbbde5c418be25138bba06bff29fdc5b9db6e1
Amulya <Amulya.kanaparthi@auropro.com>
2022-10-11 14:10:17 +0530

    task 3

---
+++ task3.py
@@ -0,0 +1,30 @@
+employees = [{
+  "name": "Tina",
+  "age": 30,
+  "birthday": "1990-03-10",
+  "job": "DevOps Engineer",
+  "address": {
+    "city": "New York",
+    "country": "USA"
+  }
+},
+{
+  "name": "Tim",
+  "age": 35,
+  "birthday": "1985-02-21",
+  "job": "Developer",
+  "address": {
+    "city": "Sydney",
+    "country": "Australia"
+  }
+}]
+
+# iterate all employees
+for employee in employees:
+    print("Name: ", employee['name'])
+    print("Job: ", employee['job'])
+    print("City: ", employee['address']['city'])       
+
+# access second employee country
+print(employees[1]['address']['country'])    
+
