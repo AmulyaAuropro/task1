083d20a697f83dded51753465f19711281fa6a9e
AmulyaAuropro <111051079+AmulyaAuropro@users.noreply.github.com>
2022-10-13 12:46:05 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -5,13 +5,13 @@ employee={
   "job : ": "DevOps Engineer"
 }
 
-employee['job'] = "Software Engineer"
+employee['job : '] = "Software Engineer"
 for key in employee.keys():
     print(key, employee[key])
 
 
    output:
-      name Tim
-      age 30
-      birthday 1990-03-10
-      job Software Engineer
+      name :  Tim
+      age :  30
+      birthday :  1990-03-10
+      job :  Software Engineer
