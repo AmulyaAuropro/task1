917d6cc502a4de1f19749e5e239047be448d7834
AmulyaAuropro <111051079+AmulyaAuropro@users.noreply.github.com>
2023-09-11 15:54:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAQBOKRNRFG62GQ43S"
-    secret_key = "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" 
+    access_key = "AKIAQBOKRNRFLLUMHHCS"
+    secret_key = "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" 
 }
 
 resource "aws_instance" "web" {
     ami           = "ami-0aa7d40eeae50c9a9"
     instance_type = "t2.micro"
-    subnet_id = "subnet-073aa277934df7bf9"
-    security_groups = ["sg-0547cad79f183323c"]
+    
 
     tags = {
       Name = "first_Instance"
